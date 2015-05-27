c6e6ec5a25beed1fe25b56caa3a169bdeb9e4d17
dranelol <cardsfan853@gmail.com>
2015-05-26 22:27:55 -0500

    whoops

---
+++ Assets/Scripts/ServerManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ServerManager: MonoBehaviour
+{
+    private static ServerManager _instance;
+
+    private void Awake()
+    {
+        if (_instance == null)
+        {
+            //If I am the first instance, make me the Singleton
+            _instance = this;
+            DontDestroyOnLoad(this);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    public static ServerManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = GameObject.FindObjectOfType<ServerManager>();
+                DontDestroyOnLoad(_instance.gameObject);
+            }
+
+            return _instance;
+        }
+    }
+
+}
